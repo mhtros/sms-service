df0e4d303fc65c67d70e496145a49409ca6ad296
mhtros <mitropanos95@gmail.com>
2022-09-14 18:56:23 +0300

    Remove mock RouteEndpoint

---
+++ SmsSendingApp/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace SmsSendingApp.Data;
+
+public class DatabaseContext
+{
+    private readonly string _connectionString;
+
+    public DatabaseContext(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+}
