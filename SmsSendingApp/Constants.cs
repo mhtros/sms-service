288ffc945e5ef7a4108fc9854f59b11711c8d994
mhtros <mitropanos95@gmail.com>
2022-09-16 17:34:40 +0300

    Global constants file

---
+++ SmsSendingApp/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace SmsSendingApp;
+
+public static class Constants
+{
+    public const ushort SmsMessageMaxLength = 480;
+    public const ushort SmsMessageChunkSize = 160;
+
+    public enum CountryCodes
+    {
+        Greece = 30,
+        Cyprus = 357
+    }
+}
