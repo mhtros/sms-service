65ec1db89c361891c0793a3eaf58c51de296feb8
mhtros <mitropanos95@gmail.com>
2022-09-16 17:42:43 +0300

    SmsVendorCy implementation

---
+++ SmsSendingApp/Services/SmsVendorCy.cs
@@ -0,0 +1,46 @@
+﻿using System.Transactions;
+using SmsSendingApp.Contracts;
+using SmsSendingApp.Entities;
+
+namespace SmsSendingApp.Services;
+
+public class SmsVendorCy : BaseVendor, IVendorStrategy
+{
+    public SmsVendorCy(IServiceProvider provider) : base(provider)
+    {
+    }
+
+    public short CountryCode => (short)Constants.CountryCodes.Cyprus;
+
+    public async Task SendAsync(Sms sms)
+    {
+        var msgLength = sms.Message.Length;
+
+        CheckIfExceedingMaxLength(msgLength);
+
+        if (msgLength <= Constants.SmsMessageChunkSize)
+        {
+            await SmsRepository.SaveAsync(sms);
+        }
+        else
+        {
+            var messageParts = sms.Message
+                .Chunk(Constants.SmsMessageChunkSize)
+                .Select(chunk => new string(chunk))
+                .ToArray();
+
+            using var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            for (short i = 0; i < messageParts.Length; i++)
+            {
+                sms.Id = default;
+                sms.Order = i;
+                sms.Message = messageParts[i];
+
+                await SmsRepository.SaveAsync(sms);
+            }
+
+            transactionScope.Complete();
+        }
+    }
+}
