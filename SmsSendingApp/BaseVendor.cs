4ce4ee8f0c1fc991bd5235b830f240abbec3c374
mhtros <mitropanos95@gmail.com>
2022-09-16 17:41:55 +0300

    IVendorStrategy, BaseVendor

---
+++ SmsSendingApp/BaseVendor.cs
@@ -0,0 +1,26 @@
+﻿using SmsSendingApp.Contracts;
+
+namespace SmsSendingApp;
+
+public abstract class BaseVendor
+{
+    protected readonly ISmsRepository SmsRepository;
+
+    protected BaseVendor(IServiceProvider provider)
+    {
+        // Passing the ISmsRepository dependency using a newly created scope with
+        // the service Locator pattern. Doing that will ensure that the ISmsRepository
+        // instances will be refreshed every time the BaseVendor class is initialized
+        // and there will be no captured dependencies (Scopes overlapping).
+        using var scope = provider.CreateScope();
+        SmsRepository = scope.ServiceProvider.GetRequiredService<ISmsRepository>();
+    }
+
+    protected static void CheckIfExceedingMaxLength(int msgLength)
+    {
+        if (msgLength > Constants.SmsMessageMaxLength)
+
+            throw new InvalidDataException(
+                $"Message exceeds the max length. [Max: {Constants.SmsMessageMaxLength}] [Message:{msgLength}]");
+    }
+}
---
+++ SmsSendingApp/Contracts/IVendorStrategy.cs
@@ -0,0 +1,14 @@
+﻿using SmsSendingApp.Entities;
+
+namespace SmsSendingApp.Contracts;
+
+public interface IVendorStrategy
+{
+    public short CountryCode { get; }
+
+    /// <summary>
+    ///     Sends an sms message to a given phone number.
+    /// </summary>
+    /// <param name="sms"> <see cref="Sms" /> to be sent.</param>
+    public Task SendAsync(Sms sms);
+}
