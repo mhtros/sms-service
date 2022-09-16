af13e337c0885325e29020bbf5810595c4ca2e64
mhtros <mitropanos95@gmail.com>
2022-09-16 17:44:21 +0300

    SmsVendorRest implementation

---
+++ SmsSendingApp/Services/SmsVendorRest.cs
@@ -0,0 +1,19 @@
+﻿using SmsSendingApp.Contracts;
+using SmsSendingApp.Entities;
+
+namespace SmsSendingApp.Services;
+
+public class SmsVendorRest : BaseVendor, IVendorStrategy
+{
+    public SmsVendorRest(IServiceProvider provider) : base(provider)
+    {
+    }
+
+    public short CountryCode => -1; // wild card.
+
+    public async Task SendAsync(Sms sms)
+    {
+        CheckIfExceedingMaxLength(sms.Message.Length);
+        await SmsRepository.SaveAsync(sms);
+    }
+}
