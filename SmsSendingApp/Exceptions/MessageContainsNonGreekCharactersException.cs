a9ffa0e6c15f7eb53768aff5aa49e1fa0f5a601e
mhtros <mitropanos95@gmail.com>
2022-09-16 17:43:36 +0300

    SmsVendorGr implementation

---
+++ SmsSendingApp/Exceptions/MessageContainsNonGreekCharactersException.cs
@@ -0,0 +1,11 @@
+﻿namespace SmsSendingApp.Exceptions;
+
+public class MessageContainsNonGreekCharactersException : Exception
+{
+    public string Message { get; }
+
+    public MessageContainsNonGreekCharactersException(string message)
+    {
+        Message = message;
+    }
+}
---
+++ SmsSendingApp/Services/SmsVendorGr.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using SmsSendingApp.Contracts;
+using SmsSendingApp.Entities;
+using SmsSendingApp.Exceptions;
+
+namespace SmsSendingApp.Services;
+
+public class SmsVendorGr : BaseVendor, IVendorStrategy
+{
+    private const string OnlyGreekRegex = @"^[(0-9.!@?#""$%&:;() *\+,\/;\-=[\\\]\^_{|}<>\p{IsGreek}+(\s)?)+]*$";
+
+    public SmsVendorGr(IServiceProvider provider) : base(provider)
+    {
+    }
+
+    public short CountryCode => (short)Constants.CountryCodes.Greece;
+
+    public async Task SendAsync(Sms sms)
+    {
+        CheckIfExceedingMaxLength(sms.Message.Length);
+
+        var onlyGreekCharacters = Regex.IsMatch(sms.Message, OnlyGreekRegex);
+        if (onlyGreekCharacters is false) throw new MessageContainsNonGreekCharactersException(sms.Message);
+
+        await SmsRepository.SaveAsync(sms);
+    }
+}
